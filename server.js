e1bcbb1865007593a43fceaffb558c7a71271646
Sitarkjs <58781687+Sitarkjs@users.noreply.github.com>
2021-01-16 19:28:01 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const { Client, Collection } = require("discord.js");
+const client = global.client = new Client({ fetchAllMembers: true });
+const config = require("./config.json");
+const fs = require("fs");
+
+client.commands = new Collection();
+client.aliases = new Collection();
+
+fs.readdirSync("./commands").filter(file => file.endsWith(".js")).forEach(file => {
+    let command = require(`./commands/${file}`);
+    client.commands.set(command.conf.command, command);
+    console.log(`[Command] ${file.replace(".js", "")} command loaded.`);
+    command.conf.aliases.forEach(aliases => {
+    client.aliases.set(aliases, command)  
+    });
+});
+
+fs.readdirSync("./events").filter(file => file.endsWith(".js")).forEach(file => {
+    let event = require(`./events/${file}`);
+    client.on(event.conf.event, event.execute);
+    console.log(`[Event] ${file.replace(".js", "")} event loaded.`);
+});
+
+client.login(config.Token).then(c => console.log(`Logged in as ${client.user.tag}!`)).catch(err => console.error(`Failed to login to the bot!`));
